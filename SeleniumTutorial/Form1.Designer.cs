03158dc277fbb50ec9961c87164d19a0f16d34c2
Adrian Alvarez <adriangalvarez@gmail.com>
2020-02-09 15:03:54 -0300

    Basic browser management (open, maximize, minimize and navigate)

--- SeleniumTutorial/Form1.Designer.cs
+++ SeleniumTutorial/Form1.Designer.cs
@@ -28,13 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnOpenBrowser = new System.Windows.Forms.Button();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.btnMaximize = new System.Windows.Forms.Button();
+            this.txtNavigate = new System.Windows.Forms.TextBox();
+            this.btnNavigate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpenBrowser
+            // 
+            this.btnOpenBrowser.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenBrowser.Name = "btnOpenBrowser";
+            this.btnOpenBrowser.Size = new System.Drawing.Size(75, 43);
+            this.btnOpenBrowser.TabIndex = 0;
+            this.btnOpenBrowser.Text = "Open Browser";
+            this.btnOpenBrowser.UseVisualStyleBackColor = true;
+            this.btnOpenBrowser.Click += new System.EventHandler(this.btnOpenBrowser_Click);
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.Location = new System.Drawing.Point(93, 12);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(75, 43);
+            this.btnMinimize.TabIndex = 1;
+            this.btnMinimize.Text = "Minimize Browser";
+            this.btnMinimize.UseVisualStyleBackColor = true;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            // 
+            // btnMaximize
+            // 
+            this.btnMaximize.Location = new System.Drawing.Point(174, 12);
+            this.btnMaximize.Name = "btnMaximize";
+            this.btnMaximize.Size = new System.Drawing.Size(75, 43);
+            this.btnMaximize.TabIndex = 2;
+            this.btnMaximize.Text = "Maximize Browser";
+            this.btnMaximize.UseVisualStyleBackColor = true;
+            this.btnMaximize.Click += new System.EventHandler(this.btnMaximize_Click);
+            // 
+            // txtNavigate
+            // 
+            this.txtNavigate.Location = new System.Drawing.Point(12, 61);
+            this.txtNavigate.Name = "txtNavigate";
+            this.txtNavigate.Size = new System.Drawing.Size(237, 20);
+            this.txtNavigate.TabIndex = 3;
+            // 
+            // btnNavigate
+            // 
+            this.btnNavigate.Location = new System.Drawing.Point(174, 87);
+            this.btnNavigate.Name = "btnNavigate";
+            this.btnNavigate.Size = new System.Drawing.Size(75, 23);
+            this.btnNavigate.TabIndex = 4;
+            this.btnNavigate.Text = "Navigate";
+            this.btnNavigate.UseVisualStyleBackColor = true;
+            this.btnNavigate.Click += new System.EventHandler(this.btnNavigate_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size( 800, 450 );
+            this.ClientSize = new System.Drawing.Size(263, 124);
+            this.Controls.Add(this.btnNavigate);
+            this.Controls.Add(this.txtNavigate);
+            this.Controls.Add(this.btnMaximize);
+            this.Controls.Add(this.btnMinimize);
+            this.Controls.Add(this.btnOpenBrowser);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnOpenBrowser;
+        private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.Button btnMaximize;
+        private System.Windows.Forms.TextBox txtNavigate;
+        private System.Windows.Forms.Button btnNavigate;
     }
 }
 
--- SeleniumTutorial/Form1.cs
+++ SeleniumTutorial/Form1.cs
@@ -12,9 +12,31 @@ namespace SeleniumTutorial
 {
     public partial class Form1 : Form
     {
+        private OpenQA.Selenium.IWebDriver driver;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnOpenBrowser_Click( object sender, EventArgs e )
+        {
+            driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+        }
+
+        private void btnMinimize_Click( object sender, EventArgs e )
+        {
+            driver.Manage().Window.Minimize();
+        }
+
+        private void btnMaximize_Click( object sender, EventArgs e )
+        {
+            driver.Manage().Window.Maximize();
+        }
+
+        private void btnNavigate_Click_1( object sender, EventArgs e )
+        {
+            driver.Navigate().GoToUrl( "https://" + txtNavigate.Text );
         }
     }
 }
